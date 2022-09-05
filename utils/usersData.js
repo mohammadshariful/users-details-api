7ec8acb61b5d370dd9fd0056446dac5e93f2de15
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-09-05 14:53:49 +0600

    something change

--- utils/usersData.js
+++ utils/usersData.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const dataPath = "users.json";
-
+//get all users
 module.exports.getUsersData = () => {
   const jsonData = fs.readFileSync(dataPath);
   return JSON.parse(jsonData);
 };
-
+//save user
 module.exports.saveUserData = (data) => {
   const stringifyData = JSON.stringify(data);
   fs.writeFileSync(dataPath, stringifyData);
